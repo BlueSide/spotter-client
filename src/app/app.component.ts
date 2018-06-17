2a1b0c389af5d5e4f1c6127c8c721c85b3120f49
cikzh <marlon@blueside.nl>
2018-06-17 23:45:26 +0200

    first iteration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,90 @@
 import { Component } from '@angular/core';
+import { TeamStat } from './team-stat/team-stat.component';
+import { BallsportsService } from './ballsports.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+
+    teamStats: any[];
+
+    connected: boolean;
+    loading: boolean;
+    matchTitle: string;
+    matchId: number;
+
+    playersTeam0: any[];
+    playersTeam1: any[];
+
+    selectedPlayerTeam0: any = {};
+    selectedPlayerTeam1: any = {};
+
+    
+    constructor(private ballsports: BallsportsService)
+    {
+        this.getData();
+    }
+
+    public getData()
+    {
+        this.loading = true;
+        this.ballsports.getAllStats().subscribe(
+            (data) => {
+                this.teamStats = data.teamStats;
+                this.matchTitle = data.title;
+                this.matchId = data.matchId;
+
+                this.playersTeam0 = data.players.filter((item, index) => {
+                    return item.id !== '0' && index < 19;
+                });
+
+                // Add the playerId and teamId to every stat
+                this.playersTeam0.forEach((player) => {
+                    player.stats.forEach((stat) => {
+                        stat.teamId = 0;
+                        stat.playerId = player.id;
+                    });
+                });
+
+                
+                this.playersTeam1 = data.players.filter((item, index) => {
+                    return item.id !== '0' && index >= 20 && index < 39;
+                });
+
+
+                // Add the playerId and teamId to every stat
+                this.playersTeam1.forEach((player) => {
+                    player.stats.forEach((stat) => {
+                        stat.teamId = 1;
+                        stat.playerId = player.id;
+                    });
+                });
+
+
+                
+                //NOTE: Select the first player of any team if none is selected
+                if(Object.keys(this.selectedPlayerTeam0).length === 0)
+                    this.selectedPlayerTeam0 = this.playersTeam0[0];
+                if(Object.keys(this.selectedPlayerTeam1).length === 0)
+                    this.selectedPlayerTeam1 = this.playersTeam1[0];
+                
+                this.connected = true;
+                this.loading = false;
+            },
+            error => {
+                //TODO: Try to reconnect
+                this.connected = false;
+                this.loading = false;
+            }
+        );
+    }
+
+    public selectPlayer(player: any, team: number)
+    {
+        if(team === 0) this.selectedPlayerTeam0 = player;
+        if(team === 1) this.selectedPlayerTeam1 = player;
+    }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 
+import { BallsportsService } from './ballsports.service';
 
 import { AppComponent } from './app.component';
-
+import { TeamStatComponent } from './team-stat/team-stat.component';
+import { PlayerStatComponent } from './player-stat/player-stat.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent,
+        TeamStatComponent,
+        PlayerStatComponent
+    ],
+    imports: [
+        BrowserModule,
+        FormsModule,
+        HttpClientModule
+    ],
+    providers: [
+        BallsportsService
+    ],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
